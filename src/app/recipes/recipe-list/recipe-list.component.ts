917243eb3ff5e58c18030733939be2c0aa294c45
vinhvinhvroom <vinh.doan13@gmail.com>
2020-03-25 13:13:42 -0400

    Recipe List

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.cookingclassy.com/wp-content/uploads/2019/07/steak-marinade-12.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.cookingclassy.com/wp-content/uploads/2019/07/steak-marinade-12.jpg'),
+    new Recipe('A Test Recipe 2', 'This is simply a test', 'https://www.cookingclassy.com/wp-content/uploads/2019/07/steak-marinade-12.jpg'),
+    new Recipe('A Test Recipe 3', 'This is simply a test', 'https://www.cookingclassy.com/wp-content/uploads/2019/07/steak-marinade-12.jpg')
   ];
   constructor() { }
 
